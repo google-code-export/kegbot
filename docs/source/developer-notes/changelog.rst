03c3578940efa8ce4bdd856a58b1626b722d29fd
mike <opensource@hoho.com>
2010-09-14 23:23:05 -0700

    kegweb: make session box a little tidier.

---
+++ docs/source/developer-notes/changelog.rst
@@ -0,0 +1,169 @@
+.. _changelog:
+
+Changelog
+=========
+
+This changelog covers all Kegbot components (pykeg, kegweb, kegboard, docs).
+
+Version 0.7.5 (2010-09-11)
+--------------------------
+
+*Note:* Due to changes to the third-party ``socialregistration`` dependency,
+existing users will need to issue the following command after updating::
+  $ kegbot_admin.py migrate --fake socialregistration 0001
+  $ kegbot_admin.py migrate
+
+*Note:* If you have installed using ``./setup.py develop``, you will need to
+issue that command again; new versions of some dependencies are required.
+
+Core / General
+^^^^^^^^^^^^^^
+* Fixed a race condition which could cause the kegbot core to crash due to an
+  erroneous watchdog error.
+* Fixed a crash in ``kegbot_admin.py kb_regen_stats`` that would occur when
+  computing stats for a keg with no recorded drinks.  (The workaround was to go
+  have a beer..)
+* Fixed issue #50 (do not record drinks below minimum volume threshold.)
+
+Kegweb
+^^^^^^
+* Updated to use ``django-socialregistration`` version 0.4.2, and the official
+  ``facebook-python-sdk`` package.  Removed mirror of pyfacebook.
+* The number of recent pours shown on the main page is now configurable.  See
+  ``KEGWEB_LAST_DRINK_COUNT`` in ``common_settings.py.example``
+
+
+Version 0.7.4 (2010-09-08)
+--------------------------
+
+Core / General
+^^^^^^^^^^^^^^
+* Backend: extensive under-the-hood changes to support multiple sites in a
+  single backend instance.  This isn't yet used by anything.
+* Fixed issue with pykeg.core migration 0031.
+* Improvements to session record keeping.
+* Added new SystemEvent table.
+
+Kegweb
+^^^^^^
+* Improved keg detail page, with better-looking sessions.
+
+
+Version 0.7.3 (2010-09-01)
+--------------------------
+
+*Note:* Existing users upgrading from a previous kegbot version will need to
+issue the migrate command to update their database schema.  Also, statistics and
+sessions need to be regenerated::
+  $ kegbot_admin.py migrate
+  $ kegbot_admin.py kb_regen_sessions
+  $ kegbot_admin.py kb_regen_stats
+
+Core / General
+^^^^^^^^^^^^^^
+* Fixed issue authentication tokens for consecutive pours not being reported
+  correctly.
+* Improved stats reporting; fixed drinker breakdown graph on keg detail page.
+* Added a notes field for Keg records.
+* Internal cleanups to the backend APIs.
+* Schema change: Started record auth token details used for each pour.
+* Schema change: Guest pours are now represented by a ``null`` user (rather than
+  a specific guest account) in the database.
+
+Kegweb
+^^^^^^
+* Fixed issue causing kegweb to break when used without proper Facebook
+  credentials.
+* Improvements to the currently undocumented kegweb API.
+
+Kegboard
+^^^^^^^^
+* Update KegShield schematics to include Arduino and Arduino Mega shield
+  designs.
+
+Version 0.7.2 (2010-06-29)
+--------------------------
+
+Core / General
+^^^^^^^^^^^^^^
+* Django v1.2 is now **required**.
+* Added new dependency on ``django_nose`` for running unittests; ``make test``
+  works once again to run unittests
+* Improved LCD UI; now shows tap status, last pour information.
+* Fixed SoundServer, which had stopped working some time ago.
+* Miscellaneous packaging fixes, which should make installation with ``pip`` work
+  a bit better.
+
+Kegweb
+^^^^^^
+* Fix for bug #48: Facebook connect login broken.
+* Fixed/update CSRF detection on forms for Django 1.2.
+* Bugfixes for the Kegweb REST ('krest') API.
+
+Twitter
+^^^^^^^
+* Moved Twitter add-on out of the core and into a new daemon,
+  ``kegbot_twitter``, similar to Facebook app ``fb_publisher``.
+
+
+Version 0.7.1 (2010-06-04)
+--------------------------
+
+Core / General
+^^^^^^^^^^^^^^
+* Added missing dependencies to `setup.py`.
+* Removed a few locally-mirrored dependencies.
+* Added protobuf source mirror to `setup.py`.
+
+Kegweb
+^^^^^^
+* Reorganized account settings views.
+* Add password reset forms.
+
+Version 0.7.0 (2010-05-23)
+--------------------------
+
+Initial numbered release! (Changes are since hg revision 500:525e06329039).
+
+Core / General
+^^^^^^^^^^^^^^
+* Vastly improved authentication device support.
+* New network protocol for Kegbot status and control (kegnet).
+* Temperatures are once again recorded. Temperature sensors can be associated
+  with a specific keg tap.
+* Support for Phidgets RFID reader.
+* Flowmeter resolution is now set on a tap-by-tap basis (in KegTap table).
+* Twitter: added config option to suppress tweets for unknown users.
+* Started using django-south for schema migrations.
+* Sound playback on flow events: added the sound_server application.
+* Added kegbot_master program, to control and monitor full suite of kegbot
+  daemons.
+* Improved support for CrystalFontz LCD devices; new support for Matrix-Orbital
+  serial LCD displays.
+* Added Facebook publisher add-on.
+* Packaging improvements; `setup.py install` works.
+
+Kegboard
+^^^^^^^^
+* Bumped firmware version to v5.
+* Fixed packet CRCs.
+* Added support for OneWire presence detect/authentication device.
+* Improved DS1820 temperature sensing.
+* Improved responsiveness of OneWire presence detect.
+* Shrunk size of firmware significantly.
+* Added experimental support for serial LCDs.
+* Added schematic files for Kegboard Arduino shield.
+
+Kegweb
+^^^^^^
+* Design refresh; new HTML/CSS and many more graphs and stats.
+* Added keg administration tab.
+* Added experimental support for Facebook connect.
+* Fixed broken relative time display.
+* Fixed bug on submitting new user registration.
+
+Docs
+^^^^
+* Improved documentation.
+* Added changelog :)
+
---
+++ pykeg/src/pykeg/web/api/views.py
@@ -0,0 +1,345 @@
+# Copyright 2010 Mike Wakerly <opensource@hoho.com>
+#
+# This file is part of the Pykeg package of the Kegbot project.
+# For more information on Pykeg or Kegbot, see http://kegbot.org/
+#
+# Pykeg is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pykeg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pykeg.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Kegweb RESTful API views."""
+
+import datetime
+from functools import wraps
+import hashlib
+import sys
+
+try:
+  import json
+except ImportError:
+  try:
+    import simplejson as json
+  except ImportError:
+    try:
+      from django.utils import simplejson as json
+    except ImportError:
+      raise ImportError, "Unable to load a json library"
+
+from django.conf import settings
+from django.http import Http404
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.template import Context
+from django.template.loader import get_template
+
+from pykeg.core import backend
+from pykeg.core import models
+from pykeg.core import protolib
+from pykeg.core import protoutil
+from pykeg.web.api import common
+from pykeg.web.api import forms
+
+from google.protobuf.message import Message
+
+INDENT = 2
+
+### Authentication
+
+AUTH_KEY_BASE = None
+AUTH_KEY = None
+if hasattr(settings, 'KEGBOT_API_AUTH_SECRET') and \
+    settings.KEGBOT_API_AUTH_SECRET:
+  AUTH_KEY_BASE = settings.KEGBOT_API_AUTH_SECRET
+elif hasattr(settings, 'SECRET_KEY') and settings.SECRET_KEY:
+  AUTH_KEY_BASE = settings.SECRET_KEY
+
+if AUTH_KEY_BASE:
+  _m = hashlib.sha256()
+  _m.update(AUTH_KEY_BASE)
+  AUTH_KEY = _m.hexdigest()
+
+### Decorators
+
+def auth_required(viewfunc):
+  def _check_token(request, *args, **kwargs):
+    # Check for api_auth_token; allow in either POST or GET arguments.
+    tok = request.REQUEST.get('api_auth_token')
+    if not tok:
+      raise common.NoAuthTokenError
+    if tok.lower() == AUTH_KEY.lower():
+      return viewfunc(request, *args, **kwargs)
+    else:
+      raise common.BadAuthTokenError
+  return wraps(viewfunc)(_check_token)
+
+def staff_required(viewfunc):
+  def _check_token(request, *args, **kwargs):
+    if not request.user or not request.user.is_staff or not \
+        request.user.is_superuser:
+      raise common.PermissionDeniedError, "Logged-in staff user required"
+    return viewfunc(request, *args, **kwargs)
+  return wraps(viewfunc)(_check_token)
+
+def ToJsonError(e):
+  """Converts an exception to an API error response."""
+  if isinstance(e, common.Error):
+    code = e.__class__.__name__
+    message = e.Message()
+  elif isinstance(e, ValueError):
+    code = 'BadRequestError'
+    message = str(e)
+  else:
+    code = 'ServerError'
+    message = 'An internal error occurred: %s' % str(e)
+  result = {
+    'error' : {
+      'code' : code,
+      'message' : message
+    }
+  }
+  return result
+
+def obj_to_dict(o, with_full=False):
+  if hasattr(o, '__iter__'):
+    return [protoutil.ProtoMessageToDict(protolib.ToProto(x, with_full)) for x in o]
+  else:
+    return protoutil.ProtoMessageToDict(protolib.ToProto(o, with_full))
+
+def py_to_json(f):
+  """Decorator that wraps an API method.
+
+  The decorator transforms the method in a few ways:
+    - The raw return value from the method is converted to a serialized JSON
+      result.
+    - The result is wrapped in an outer dict, and set as the value 'result'
+    - If an exception is thrown during the method, it is converted to a protocol
+      error message.
+  """
+  def new_function(*args, **kwargs):
+    try:
+      try:
+        result = {'result' : f(*args, **kwargs)}
+      except Http404, e:
+        # We might change the HTTP status code here one day.  This also allows
+        # the views to use Http404 (rather than NotFound).
+        raise common.NotFoundError(e.message)
+      data = json.dumps(result, indent=INDENT)
+    except Exception, e:
+      result = ToJsonError(e)
+      data = json.dumps(result, indent=INDENT)
+    return HttpResponse(data, mimetype='application/json')
+  return new_function
+
+def model_to_py(f):
+  """Decorator which converts method results to native types.
+
+  The wrapped function should return either a single Django model instance, an
+  iterable of the same, or a protocol buffer Message.
+
+  This is a bit roundabount and inefficient:  Django model instances are first
+  converted to protocol buffers, then to base python types, and finally to a
+  JSON-formatted string.
+
+  TODO(mikey): revisit this layering as the API matures, or once a kegbot site
+  needs to handle >0.1 qps :)
+  """
+  def new_function(*args, **kwargs):
+    res = f(*args, **kwargs)
+    request = args[0]
+    with_full = False
+    if request.GET.get('full') == '1':
+      with_full = True
+    return obj_to_dict(res, with_full)
+  return new_function
+
+def model_to_json(f):
+  """Decorator that combines py_to_json and model_to_py."""
+  @py_to_json
+  @model_to_py
+  def new_function(*args, **kwargs):
+    return f(*args, **kwargs)
+  return new_function
+
+### Helpers
+
+def _get_last_drinks(request, limit=5):
+  return request.kbsite.drinks.valid()[:limit]
+
+def _form_errors(form):
+  ret = {}
+  for field in form:
+    if field.errors:
+      name = field.html_name
+      ret[name] = []
+      for error in field.errors:
+        ret[name].append(error)
+  return ret
+
+### Endpoints
+
+@model_to_json
+def last_drinks(request, limit=5):
+  return _get_last_drinks(request, limit)
+
+@model_to_json
+def all_kegs(request):
+  return request.kbsite.keg_set.all().order_by('-startdate')
+
+@model_to_json
+@auth_required   # for now, due to expense; TODO paginate me
+def all_drinks(request):
+  return models.Drink.objects.all().order_by('id')
+
+@model_to_json
+def get_drink(request, drink_id):
+  return get_object_or_404(models.Drink, pk=drink_id, site=request.kbsite)
+
+@model_to_json
+def get_keg(request, keg_id):
+  return get_object_or_404(models.Keg, pk=keg_id, site=request.kbsite)
+
+@model_to_json
+def get_keg_drinks(request, keg_id):
+  keg = get_object_or_404(models.Keg, pk=keg_id, site=request.kbsite)
+  return list(keg.drinks.valid())
+
+@model_to_json
+def all_taps(request):
+  return request.kbsite.kegtap_set.all().order_by('name')
+
+@model_to_json
+def get_user(request, username):
+  return get_object_or_404(models.User, username=username)
+
+@model_to_json
+def get_user_drinks(request, username):
+  user = get_object_or_404(models.User, username=username)
+  return list(user.drinks.valid())
+
+@model_to_json
+def get_auth_token(request, auth_device, token_value):
+  return get_object_or_404(models.AuthenticationToken, auth_device=auth_device,
+      token_value=token_value)
+
+@model_to_json
+def all_thermo_sensors(request):
+  return list(request.kbsite.thermosensors.all())
+
+def _get_sensor_or_404(request, sensor_name):
+  try:
+    sensor = models.ThermoSensor.objects.get(site=request.kbsite,
+        raw_name=sensor_name)
+  except models.ThermoSensor.DoesNotExist:
+    try:
+      sensor = models.ThermoSensor.objects.get(site=request.kbsite,
+          nice_name=sensor_name)
+    except models.ThermoSensor.DoesNotExist:
+      raise Http404
+  return sensor
+
+def get_thermo_sensor(request, sensor_name):
+  if request.method == 'POST':
+    return thermo_sensor_post(request, sensor_name)
+  else:
+    return thermo_sensor_get(request, sensor_name)
+
+@model_to_json
+def thermo_sensor_get(request, sensor_name):
+  sensor = _get_sensor_or_404(request, sensor_name)
+  return sensor
+
+@py_to_json
+@auth_required
+def thermo_sensor_post(request, sensor_name):
+  sensor = _get_sensor_or_404(request, sensor_name)
+  form = forms.ThermoPostForm(request.POST)
+  if not form.is_valid():
+    raise common.BadRequestError, _form_errors(form)
+  cd = form.cleaned_data
+  b = backend.KegbotBackend(site=request.kbsite)
+  # TODO(mikey): use form fields to compute `when`
+  res = b.LogSensorReading(sensor.raw_name, cd['temp_c'])
+  return protoutil.ProtoMessageToDict(res)
+
+@model_to_json
+def get_thermo_sensor_logs(request, sensor_name):
+  sensor = _get_sensor_or_404(request, sensor_name)
+  logs = sensor.thermolog_set.all()[:60*2]
+  return logs
+
+@py_to_json
+def last_drinks_html(request, limit=5):
+  last_drinks = _get_last_drinks(request, limit)
+
+  # render each drink
+  template = get_template('kegweb/drink-box.html')
+  results = []
+  for d in last_drinks:
+    row = {}
+    row['id'] = d.id
+    row['box_html'] = template.render(Context({'drink': d}))
+    results.append(row)
+  return results
+
+@py_to_json
+def last_drink_id(request):
+  last = _get_last_drinks(request, limit=1)
+  if not last.count():
+    return {'id': 0}
+  else:
+    return {'id': last[0].id}
+
+@py_to_json
+@staff_required
+def get_access_token(request):
+  return {'token': AUTH_KEY}
+
+def tap_detail(request, tap_id):
+  if request.method == 'POST':
+    return tap_detail_post(request, tap_id)
+  else:
+    return tap_detail_get(request, tap_id)
+
+@model_to_json
+def tap_detail_get(request, tap_id):
+  return get_object_or_404(models.KegTap, meter_name=tap_id, site=request.kbsite)
+
+@py_to_json
+@auth_required
+def tap_detail_post(request, tap):
+  form = forms.DrinkPostForm(request.POST)
+  if not form.is_valid():
+    raise common.BadRequestError, _form_errors(form)
+  cd = form.cleaned_data
+  if cd.get('pour_time') and cd.get('now'):
+    pour_time = datetime.datetime.fromtimestamp(cd.get('pour_time'))
+    now = datetime.datetime.fromtimestamp(cd.get('now'))
+    skew = datetime.datetime.now() - now
+    pour_time += skew
+  else:
+    pour_time = None
+  b = backend.KegbotBackend(site=request.kbsite)
+  try:
+    res = b.RecordDrink(tap_name=tap,
+      ticks=cd['ticks'],
+      volume_ml=cd.get('volume_ml'),
+      username=cd.get('username'),
+      pour_time=pour_time,
+      duration=cd.get('duration'),
+      auth_token=cd.get('auth_token'))
+  except backend.BackendError, e:
+    raise common.ServerError(str(e))
+  return protoutil.ProtoMessageToDict(res)
+
+@py_to_json
+def default_handler(request):
+  raise Http404, "Not an API endpoint: %s" % request.path[:100]
