dfe501fc4476992fbcda5cd07feb0c5708930984
mike <opensource@hoho.com>
2010-12-13 14:26:32 -0500

    core: rotate logs every night at midnight when logging to file.

---
+++ docs/source/index.rst
@@ -0,0 +1,69 @@
+.. This is the index of kegbot documentation.  These files are best viewed in
+   HTML format at http://kegbot.org/docs/
+
+Welcome to Kegbot's documentation!
+==================================
+
+This is the central index of official Kegbot documentation.
+
+Several guides have been published; see abridged outlines below, or click on the
+document titles for the full document. These documents are also included in the
+pykeg software distribution.
+
+Contents:
+
+.. toctree::
+  :maxdepth: 2
+
+  user-guide/index
+  kegboard-guide/index
+  api/index
+  developer-notes/index
+
+
+
+Other Resources
+===============
+
+Sadly, you may find some of this documentation incomplete. Our apologies! We're
+still working on improving and completing the Kegbot documentation.
+
+If the published documentation hasn't solved your issue, you may have luck with
+these other Kegbot resources:
+
+**KegBB Forums**
+  The `KegBB Forums
+  <http://kegbot.org/kegbb/>`_ may or may not
+  be a vibrant community for help and exchanging ideas.
+
+**Bug Tracker**
+  Please use our `issue tracker <http://code.google.com/p/kegbot/issues/list>`_
+  to search for and file new bugs.
+
+**Kegbot wiki**
+  The `Kegbot Wiki <http://kegbot.org/wiki>`_ is something of a scratchpad of
+  notes and ideas; anyone can add and edit information on the wiki.
+
+  Unlike the official, published docs (which are intended to describe stable
+  features and Kegbot releases), the wiki is a place for bleeding-edge notes and
+  ideas (some of which may wind up here.)
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+Documentation Wishlist
+======================
+
+Things I would love to have documented but haven't gotten around to:
+
+* Spellcheck the docs :-)
+* Allow comments/annotations (similar to django and php project docs)
+* Add developer's guide -- submitting patches, python style (or lack thereof),
+  etc.
+* Describe how to use an OpenWRT box as a kegboard controller.
+
---
+++ pykeg/src/pykeg/core/kb_common.py
@@ -0,0 +1,88 @@
+from pykeg.core import util
+
+### Drink-related constants
+
+# Don't record teeny drinks
+MIN_VOLUME_TO_RECORD = 10
+
+# Don't show small drinks
+MIN_VOLUME_TO_DISPLAY = 10
+
+# Minimum session size, in milliliters, to display
+MIN_SESSION_VOLUME_DISPLAY_ML = 177  # 6 ounces
+
+# Maximum time between consecutive drinks to be considered in the same 'session'
+# (see UserDrinkingSession table)
+DRINK_SESSION_TIME_MINUTES = 90
+
+# Maximum time between different UserDrinkingSession records to be considered
+# 'concurrent' (see DrinkingSessionGroup table)
+GROUP_SESSION_TIME_MINUTES = 90
+
+# The maximum difference between consecutive meter readings that is considered
+# valid.
+MAX_METER_READING_DELTA = 2200*2
+
+# Minimum and maximum thermo sensor readings (degrees C).
+THERMO_SENSOR_RANGE = (-20.0, 80.0)
+
+# Address the kegnet server should bind to.
+KB_CORE_DEFAULT_ADDR = 'localhost:9805'
+
+DEFAULT_NEW_USER_WEIGHT = 140
+DEFAULT_NEW_USER_GENDER = 'male'
+
+# String name for all taps
+ALIAS_ALL_TAPS = '__all_taps__'
+
+# Device names
+AUTH_MODULE_CORE_ONEWIRE = 'core.onewire'
+AUTH_MODULE_CONTRIB_PHIDGET_RFID = 'contrib.phidget.rfid'
+
+# Flag which determines whether an auth device is captive or non-captive.  A
+# captive device is one which captures the authentication token, and provides a
+# very reliable signal when the token is detached.
+#
+# For a device marked as captive, the AuthenticationManager will immediately end
+# any active flows when a token is removed.  For non-captive (or contactless)
+# devices, such as an RFID reader, the authentication manager does nothing when
+# the token is removed (see flow timeout, next).
+AUTH_DEVICE_CAPTIVE = {
+  AUTH_MODULE_CORE_ONEWIRE: True,
+  AUTH_MODULE_CONTRIB_PHIDGET_RFID: False,
+  'default': True
+}
+
+# Maximum idle time for new flows, based on initiating auth device.  "Idle" is
+# defined as seconds elapsed without any flow meter activity.
+#
+# This varies on a per-auth-device basis due to the distinction between captive
+# and non-captive devices: we want flows initiated with a contactless auth
+# device, like an RFID, to timeout sooner.
+AUTH_DEVICE_MAX_IDLE_SECS = {
+  AUTH_MODULE_CORE_ONEWIRE: 120,
+  AUTH_MODULE_CONTRIB_PHIDGET_RFID: 20,
+  'default': 10
+}
+
+# How often to record a thermo reading?
+THERMO_RECORD_DELTA_SECONDS = 60
+
+### Addresses and ports
+
+# Default port for JSON server
+JSON_SERVER_PORT_DEFAULT = 9100
+
+### Enums
+
+KB_DEVICE_TYPE = util.Enum(*(
+  'DEVICE_TYPE_UNKNOWN',
+  'DEVICE_TYPE_FLOW_DEV',
+  'DEVICE_TYPE_RELAY',
+  'DEVICE_TYPE_THERMO',
+))
+
+### Exceptions
+
+class ConfigurationError(Exception):
+  """Raised when the system is misconfigured"""
